15e73716a366bfc504e12ee7cb1a980f466153d2
Vander Costa <vander.costa@meta.com.br>
2026-09-15 21:56:10 -0300

    feat: implement WelcomeSectionComponent layout

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.css']
+  styleUrls: ['./main-content.component.css'],
+  imports: [WelcomeSectionComponent],
 })
 export class MainContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
