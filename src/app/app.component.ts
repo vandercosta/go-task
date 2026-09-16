70ed997759c1a2780b21b75eb507ebe4a7358906
Vander Costa <vander.costa@meta.com.br>
2026-09-16 15:01:43 -0300

    feat: implement TaskFormModalComponent layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainContentComponent],
+  imports: [HeaderComponent, MainContentComponent, TaskFormModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
