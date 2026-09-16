47d9dbd8b975e99e5dae0d9783d9f743f6cab85d
Vander Costa <vander.costa@meta.com.br>
2026-09-16 13:45:16 -0300

    feat: implement TaskCardComponent layout

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-task-list-section',
   templateUrl: './task-list-section.component.html',
-  styleUrls: ['./task-list-section.component.css']
+  styleUrls: ['./task-list-section.component.css'],
+  imports: [TaskCardComponent],
 })
 export class TaskListSectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
