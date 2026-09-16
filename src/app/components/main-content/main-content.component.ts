3c631f35e04ad9e63270635db8ea6529bb03687c
Vander Costa <vander.costa@meta.com.br>
2026-09-16 13:16:34 -0300

    feat: implement TaskListSectionComponent layout

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
+import { TaskListSectionComponent } from '../task-list-section/task-list-section.component';
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css'],
-  imports: [WelcomeSectionComponent],
+  imports: [WelcomeSectionComponent, TaskListSectionComponent],
 })
 export class MainContentComponent implements OnInit {
   constructor() {}
